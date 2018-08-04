1c75c84962c08dba2d57714e837362a435124ea9
LucaFlorin <luca.florin.adrian@gmail.com>
2018-08-04 14:14:02 +0300

    Progress1

---
+++ CollectiveMinds.js
@@ -0,0 +1,50 @@
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 250) {
+            $(".sector1").addClass("active");
+        } else {
+           $(".sector1").removeClass("active");
+        }
+
+        if($(window).scrollTop() > 250) {
+            $('.header_list').addClass('scroll_header');
+        }
+        else{
+            $('.header_list').removeClass('scroll_header');
+        }
+
+
+        if($(window).scrollTop() > 250) {
+            $('.logo').addClass('scroll_header');
+        }
+        else{
+            $('.logo').removeClass('scroll_header');
+        }
+
+
+        
+
+        if($(window).scrollTop() > 250) {
+            $('.socials_list').addClass('scroll_header');
+        }
+        else{
+            $('.socials_list').removeClass('scroll_header');
+        }
+
+
+
+        
+    });
+
+    $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
